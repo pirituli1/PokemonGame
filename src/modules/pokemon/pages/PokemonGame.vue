122b7177f9b6c56fc6ced95b3cfef25c29805c43
Bautista Velasco Carlos Enrique <150282401+pirituli1@users.noreply.github.com>
2024-07-23 14:53:40 -0600

    first commit

---
+++ src/modules/pokemon/pages/PokemonGame.vue
@@ -0,0 +1,20 @@
+<template>
+  <section v-if="false" class="flex flex-col justify-center items-center w-screen h-screen">
+    <h1 class="text-3xl">Espere por favor</h1>
+    <h3 class="animate-pulse">Cargando Pókemon</h3>
+  </section>
+
+  <section class="flex flex-col justify-center items-center w-screen h-screen">
+    <h1 class="m-5">¿Quién es este pókemon?</h1>
+
+    <!-- Pokemon picture-->
+    <PokemonPicture />
+    <!--Pokemon option -->
+    <PokemonOptions />
+  </section>
+</template>
+
+<script setup lang="ts">
+import PokemonOptions from '../components/PokemonOptions.vue';
+import PokemonPicture from '../components/PokemonPicture.vue';
+</script>
